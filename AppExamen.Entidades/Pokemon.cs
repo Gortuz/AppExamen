a699e99ead9605a34f704904b9e976d47dacfa38
Gortuz <alansandovalproject@gmail.com>
2024-05-15 10:39:49 -0500

    Creación de clases para base de datos de pokemon.

---
+++ AppExamen.Entidades/Pokemon.cs
@@ -0,0 +1,17 @@
+﻿namespace AppExamen.Entidades
+{
+    public class Pokemon
+    {
+        public int Id { get; set; }
+        public string Nombre { get; set; }
+        public double Peso { get; set; }
+
+        public int Nivel { get; set; }
+        public string Descripcion { get; set; }
+        public string Tipo_1 { get; set; }
+        public string Tipo_2 { get; set; }
+
+        public int NaturalezaId { get; set; }
+        public Naturaleza? Naturaleza { get; set; }
+    }
+}
